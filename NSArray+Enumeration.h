1fef7c6015846838b0f358e5eca263eb97f6724a
joshmotley <joshkmotley@gmail.com>
2016-01-26 22:07:32 -0500

    create methods that take blocks of code/iterate over arrays

---
+++ NSArray+Enumeration.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Enumeration.h
+//  
+//
+//  Created by Joshua Motley on 1/26/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Enumeration)
+
+@end
---
+++ NSArray+Enumeration.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+Enumeration.m
+//  
+//
+//  Created by Joshua Motley on 1/26/16.
+//
+//
+
+#import "NSArray+Enumeration.h"
+
+@implementation NSArray (Enumeration)
+
+@end
